6f203740a28951ed9e969d4b0cb30e69b5037509
Leonardo Cardoso <leocardosoti@gmail.com>
2017-06-06 11:54:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  
+  $("#inpt_search, #enter").on('focus', function() {
+    $(this).parent('label').addClass('active');
+    $("#enter").text("Enter to search");
+  });
+
+  $("#inpt_search").on('blur', function() {
+    if ($(this).val().length == 0)
+      $(this).parent('label').removeClass('active');
+
+    $("#enter").text("Hover to search");
+  });
+
+  $("#inpt_search").keypress(function(e) {
+    if (e.which == 13) {
+      buscar();
+    }
+  });
+  
+  $("#enter").on("click", function(){
+    buscar();
+  });
+  
+  function buscar(){
+    $(".content").remove();
+    var busca = $('#inpt_search').val();
+
+    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&uselang=user&list=search&srsearch=" + busca + "&srprop=snippet&format=json&callback=?", function(data) {
+      var link = "https://en.wikipedia.org/?title=";
+      var search = data.query.search;
+      var title = [],
+      snippet = [];
+      for (var i in search) {
+        snippet.push(search[i].snippet);
+        title.push(search[i].title);
+        $('section').append("<div class=\"row content\"><a href=\'" + link + title + "' target = \"_blank\"><h2>" + title[i] + "</h2></br><p>" + snippet[i] + "</p></div>");
+      }
+
+    });
+
+  }
+});
